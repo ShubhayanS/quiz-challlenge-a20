0d9468bd00efb58126fef8a870c31feda3e6d244
ShubhayanS <ss3902@srmist.edu.in>
2020-04-08 19:41:02 +0530

    first commit

--- app.py
+++ app.py
@@ -8,40 +8,40 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 @app.route('/home', methods=["GET", "POST"])
 def home():
-    global s
+    # global s
     if request.method == "POST":
         myDict = request.form  
         s1 = float(myDict['s1'])
         if (s1 < 10):
             link="https://www.youtube.com/watch?v=OGgG4nH8vOo&t=19s+%28carnival+A%2719%29"
-            s=666
+            # s=666
         elif(s1<20):
             link="https://www.youtube.com/watch?v=34eDWAL8dIM&t=88s+%28Main+Video+A%2719%29"
-            s=456
+            # s=456
         elif(s1<30):
             link = "https://www.instagram.com/tv/B9eMFXcjFOk/?igshid=wu6az47qkhs2"
-            s=787
+            # s=787
         elif(s1<40):
             link ="https://www.instagram.com/tv/B-R1nj9jl1z/?igshid=1a8481gk62q5r"
-            s=385
+            # s=385
         elif(s1<50):
             link ="https://www.instagram.com/p/B9s86BDjgZM/?igshid=15pcz98zt8b8i"
-            s=65 
+            # s=65 
         elif(s1<60):
             link ="https://www.instagram.com/p/B-juME0DGEP/?igshid=xzs9qzvnm9f4"
-            s=143 
+            # s=143 
         elif(s1<70):
             link ="https://www.instagram.com/p/B-jGO0ZDohX/?igshid=1ktqlefhsp1pp"
-            s=982 
+            # s=982 
         elif(s1<80):
             link ="https://www.instagram.com/p/B-hhuPGjWIF/?igshid=97ru6fhnly1l"
-            s=369
+            # s=369
         elif(s1<90):
             link ="https://www.instagram.com/p/B-gnCQ6jlw4/?igshid=17n06e6hhsbq4"
-            s=211 
+            # s=211 
         elif(s1<100):
             link ="https://www.instagram.com/p/B-RbgWJjrNf/?igshid=17ifv17lwdkk1"
-            s=420  
+            # s=420  
         else:
             return render_template('wrong.html')
         return render_template('quiz1.html',link=link)
@@ -52,12 +52,16 @@ def home():
 @app.route('/check',methods=["GET", "POST"])
 def check():
     global n
-    if (s==666 or s==456 or s==787 or s==385 or s==65 or s==143 or s== 982 or s==369 or s==211 or s==420):
-        n=random.randint(1,10)
-        return render_template('quiz2.html',num=n)
-    else:
-        return render_template('wrong2.html')
-    return render_template('wrong.html')
+    if request.method == "POST":
+        myDict2 = request.form  
+        s = int(myDict2['s'])
+        # return render_template('quiz2.html',s=s)
+        if (s==666 or s==456 or s==787 or s==385 or s==65 or s==143 or s== 982 or s==369 or s==211 or s==420):
+            n=random.randint(1,10)
+            return render_template('quiz2.html',num=n,s=s)
+        else:
+            return render_template('wrong2.html')
+    # return render_template('wrong.html')
 
 
 
@@ -66,7 +70,7 @@ def about():
     if request.method == "POST":
         myDict = request.form  
         s2 = myDict['s2']
-        if (n==1 and s2 =="27"):
+        if (n==1 and s2 =="31"):
             return render_template('yay.html')
         elif(n==2 and s2 == "7"):
             return render_template('yay.html')
